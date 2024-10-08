bd05e2a1137caca40cb791ce8008a3b23198e19c
maxwell <maxwell@aistechnolabs.org>
2024-10-08 18:05:29 +0530

    blog page making

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,12 +33,6 @@ export default function RootLayout({
         }
       `}</style>
       <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossOrigin="anonymous"
-        />
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" />
       </head>
       <body >
